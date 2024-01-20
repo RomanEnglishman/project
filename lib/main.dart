7e0618fa4b5b8d2cb8e55639ebc85fac3cf67ac4
Englishman <56271563+RomanEnglishman@users.noreply.github.com>
2024-01-21 01:38:55 +0600

    test

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class _MyAppState extends State<MyApp> {
                 color: Colors.blue,
                 padding: EdgeInsets.all(35),
                 margin: EdgeInsets.all(20),
-                width: 3200,
+                width: 32000,
                 height: 220,
               )
             ],
